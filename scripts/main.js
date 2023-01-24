9e031e6513b9d96b40b4961b892ea7e63fe0bfcf
zigsong <zigsong@woowahan.com>
2023-01-24 18:31:24 +0900

    feat: timestamp 클릭 시 플레이어 하단에 표시, 되돌아가기 기능 구현

--- scripts/main.js
+++ scripts/main.js
@@ -7,47 +7,67 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("DOMContentLoaded");
 });
 
-const handleClickTimeStamp = (commentNode) => {
-  console.log("wow!", commentNode.textContent);
+const handleClickTimeStamp = (commentNode, originContainer) => {
+  // MARK: copy할 수 있는 방안을 물색...
+  // const copiedNode = commentNode.cloneNode(true);
+  const bottomArea = document.querySelector("div#below");
+  const toastContainer = document.createElement("div");
+
+  toastContainer.setAttribute(
+    "style",
+    "width: 100%; background: rgba(255, 255, 255, 0.1); margin-top: 12px; padding: 12px; box-sizing: border-box; border-radius: 12px; cursor: pointer;"
+  );
+
+  toastContainer.appendChild(commentNode);
+  bottomArea.insertBefore(toastContainer, bottomArea.firstChild);
+
+  toastContainer.addEventListener("click", () => {
+    originContainer.insertBefore(commentNode, originContainer.firstChild);
+    originContainer.scrollIntoView({ behavior: "smooth" });
+    toastContainer.remove();
+  });
 };
 
 const observeContents = (container) => {
-  console.log("container", container);
-
   // commentsContentObserver.observe(container, {
   //   childList: true,
   //   subtree: true,
   // });
 
+  // TODO: setTimeout 삭제하고 개선하기
   setTimeout(() => {
-    const comments = container.querySelectorAll("#content-text");
+    const comments = container.querySelectorAll("#comment-content");
     comments.forEach((comment) => {
-      // console.log(comment.textContent);
-      const timestamps = comment.querySelectorAll("a");
+      const content = comment.querySelector("#content");
+      const timestamps = content.querySelectorAll("a");
+
       if (timestamps.length > 0) {
         timestamps.forEach((timestamp) => {
           if (!timestamp.textContent.startsWith("#")) {
             timestamp.style.color = "orange";
             timestamp.addEventListener("click", () => {
-              console.log("여기", comment.textContent);
-              handleClickTimeStamp(comment);
+              // console.log("comment", comment);
+              handleClickTimeStamp(content, comment.querySelector("#expander"));
             });
           }
         });
-        // console.log(timestamp.textContent);
       }
     });
   }, 2000);
 };
 
+// MARK: MutationObserver가 2번 호출되는 버그 방지
+let isContainerLoaded = false;
+
 const commentsContainerLoaded = (mutationsList, observer) => {
   for (const mutation of mutationsList) {
     if (mutation.target.id === "comments") {
-      const commentsContainer = mutation.target.querySelector("#contents");
-      // console.log("commentsContainer:", commentsContainer);
+      if (isContainerLoaded) return;
 
+      const commentsContainer = mutation.target.querySelector("#contents");
       if (commentsContainer) {
         observeContents(commentsContainer);
+        isContainerLoaded = true;
       }
 
       observer.disconnect();
@@ -56,8 +76,6 @@ const commentsContainerLoaded = (mutationsList, observer) => {
 };
 
 const commentsContentLoaded = (mutationsList, observer) => {
-  console.log("commentsContentLoaded");
-
   for (const mutation of mutationsList) {
     console.log(mutation.target.id);
 
@@ -74,4 +92,4 @@ const commentsContentLoaded = (mutationsList, observer) => {
 };
 
 const commentsContainerObserver = new MutationObserver(commentsContainerLoaded);
-const commentsContentObserver = new MutationObserver(commentsContentLoaded);
+// const commentsContentObserver = new MutationObserver(commentsContentLoaded);
